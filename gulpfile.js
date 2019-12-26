ea3e8c0014a196e447c56b42ae3b70894acabff5
NИ <Ko2dooDev@gmail.com>
2019-12-26 17:52:49 +0500

    Update вечерний

--- gulpfile.js
+++ gulpfile.js
@@ -162,4 +162,4 @@ gulp.task('default',
      gulp.parallel('clear-cache', 'fonts', 'sass', 'css-lib', 'smart-grid', 'scripts', 'browser-sync', 'watch'));
 
 gulp.task('build',
-     gulp.series('clean', 'clear-cache', 'css-min', 'media-queries', 'prebuild', 'img'));
+     gulp.series('clean', 'clear-cache', 'media-queries', 'css-min', 'prebuild', 'img'));
