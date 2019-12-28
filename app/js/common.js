c6306b68df5b899c18c2cac148763f0b1cbb1f44
NИ <Ko2dooDev@gmail.com>
2019-12-28 10:39:28 +0500

    Update: 28.12.19 10:38 -> добавил slick

--- app/js/common.js
+++ app/js/common.js
@@ -53,4 +53,31 @@ $(function(){
 			}, 1000);
 	});
 
+	// инициализируем слайдер и настраиваем его:
+	$('.slider-block').slick({
+	  centerMode: true,
+	  centerPadding: '60px',
+	  slidesToShow: 3,
+	  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+
 });
